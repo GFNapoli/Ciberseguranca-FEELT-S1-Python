f0ff80ed9986a21fb64686c6441f561a37e80850
Gustavo <gnkenshin@gmail.com>
2026-09-11 20:31:48 -0300

    ajuste ativos

--- S1_05/ativos.py
+++ S1_05/ativos.py
@@ -1,6 +1,6 @@
-id_ativo = int(input('Por favor digite o ID do ativo: '))
-nome_ativo = input('Por favor digite o nome do ativo: ')
-nome_responsavel = input('Por favor digite o nome do responsavel por esse ativo: ')
+id_ativo = 15653
+nome_ativo = 'Teclado'
+nome_responsavel = 'Gustavo'
 
 print('---------------||---------------')
 print('O ID do ativo é: {}\nO nome do ativo é: {}\nE o responsavel é: {}'.format(id_ativo,nome_ativo, nome_responsavel))
